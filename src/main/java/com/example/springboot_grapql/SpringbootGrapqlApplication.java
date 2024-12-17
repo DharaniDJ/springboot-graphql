bdfd730eab2d1b2fd9ea9ea2b8ed71ed51e79fad
DharaniDJ <nvchinta@asu.edu>
2024-12-16 18:12:14 -0600

    first commit

---
+++ src/main/java/com/example/springboot_grapql/SpringbootGrapqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_grapql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrapqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGrapqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_grapql/SpringbootGrapqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_grapql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGrapqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
